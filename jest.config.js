c4cc9fdaf8760a2ca0c43fbe0b2e7b95879e7cf4
Patrick <trick.developer@gmail.com>
2021-05-05 02:04:19 -0400

    manually add path aliases instead of using tsconfig.json

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 const { pathsToModuleNameMapper } = require('ts-jest/utils');
-const { compilerOptions } = require('./tsconfig.json');
+//const { compilerOptions } = require('./tsconfig.json');
+const tsConfigPaths = {
+    '@/*': ['src/*'],
+    '@tests/*': ['tests/*'],
+};
 
 module.exports = {
     preset: 'ts-jest/presets/js-with-ts',
@@ -10,7 +14,7 @@ module.exports = {
     testRegex: '(/__test__/.*|/tests/.*|(\\.|/)(test|spec))\\.[tj]sx?$',
     testPathIgnorePatterns: ['/node_modules/', '/dist/'],
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
-    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: `${__dirname}/` }),
+    moduleNameMapper: pathsToModuleNameMapper(tsConfigPaths, { prefix: `${__dirname}/` }),
 
     coverageDirectory: './coverage',
     coverageReporters: ['html', 'text'],
