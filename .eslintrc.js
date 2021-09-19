9c0a2a73c3dc1b063692f2fb190ce4df16493024
stratus-sync <stratus-sync@stratus.pfizer.com>
2021-09-19 07:21:41 -0400

    config cleanup/tweaks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-    parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+    parser: '@typescript-eslint/parser',
     parserOptions: {
-        ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
-        sourceType: 'module', // Allows for the use of imports
+        ecmaVersion: 2020,
+        sourceType: 'module',
     },
     env: {
         node: true,
@@ -11,19 +11,18 @@ module.exports = {
     },
     settings: {},
     extends: [
-        'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+        'plugin:@typescript-eslint/recommended',
         'eslint:recommended',
-        'prettier',
-        'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+        // 'prettier',
+        // 'plugin:prettier/recommended',
     ],
     rules: {
-        // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
         '@typescript-eslint/explicit-function-return-type': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/no-var-requires': 'off',
         '@typescript-eslint/ban-ts-comment': 'off',
-        //'newline-per-chained-call': ['error', { ignoreChainWithDepth: 2 }],
+        'newline-per-chained-call': ['error', { ignoreChainWithDepth: 2 }],
         indent: ['error', 4, { SwitchCase: 1 }],
     },
 };
