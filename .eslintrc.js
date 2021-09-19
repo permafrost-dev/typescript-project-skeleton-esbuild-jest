0d14b00538a299e0835a0ac3e9aceeef6feeebf5
stratus-sync <stratus-sync@stratus.pfizer.com>
2021-09-19 07:06:19 -0400

    update config files, add madge config

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,7 @@ module.exports = {
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
         //'newline-per-chained-call': ['error', { ignoreChainWithDepth: 2 }],
         indent: ['error', 4, { SwitchCase: 1 }],
     },
