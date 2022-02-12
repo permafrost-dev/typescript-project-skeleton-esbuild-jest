b2677b48448d979be94103c213cf837f54e95a9c
patinthehat <patinthehat@users.noreply.github.com>
2022-02-12 04:24:30 +0000

    Fix styling

--- configure-package.js
+++ configure-package.js
@@ -175,9 +175,7 @@ const conditionalAsk = async (obj, propName, onlyEmpty, prompt, allowEmpty = fal
 };
 
 const populatePackageInfo = async (onlyEmpty = false) => {
-    const remoteUrlParts = gitCommand('config remote.origin.url').trim()
-        .replace(':', '/')
-        .split('/');
+    const remoteUrlParts = gitCommand('config remote.origin.url').trim().replace(':', '/').split('/');
 
     console.log();
 
@@ -456,10 +454,7 @@ async function configureOptionalFeatures() {
 
 const askBooleanQuestion = async str => {
     const resultStr = await askQuestion(`${str} `);
-    const result = resultStr.toString().toLowerCase()
-        .replace(/ /g, '')
-        .replace(/[^yn]/g, '')
-        .slice(0, 1);
+    const result = resultStr.toString().toLowerCase().replace(/ /g, '').replace(/[^yn]/g, '').slice(0, 1);
 
     return result === 'y';
 };
