22b1923d4c1478b8c3eb78fe02c72eed1a2f3a62
Patrick Organ <patrick@permafrost.dev>
2022-03-12 01:59:11 -0500

    add eslint jest plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,14 +10,23 @@ module.exports = {
         commonjs: true,
     },
     settings: {},
-    extends: ['plugin:@typescript-eslint/recommended', 'eslint:recommended'],
+    plugins: [
+        'jest',
+    ],
+    extends: [
+        'plugin:jest/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'eslint:recommended',
+    ],
     rules: {
         '@typescript-eslint/explicit-function-return-type': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/no-var-requires': 'off',
         '@typescript-eslint/ban-ts-comment': 'off',
+        'jest/no-disabled-tests': 'warn',
+        'jest/no-identical-title': 'error',
         'newline-per-chained-call': ['error', { ignoreChainWithDepth: 2 }],
-        indent: ['error', 4, { SwitchCase: 1 }],
+        'indent': ['error', 4, { SwitchCase: 1 }],
     },
 };
