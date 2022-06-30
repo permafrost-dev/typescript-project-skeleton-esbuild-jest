d7944c5863ab818e68f2c296d0bb638f12be224c
Patrick Organ <patrick@permafrost.dev>
2022-06-30 05:11:35 -0400

    change ts-jest/utils to ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,33 @@
-const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { pathsToModuleNameMapper } = require('ts-jest');
 //const { compilerOptions } = require('./tsconfig.json');
 const tsConfigPaths = {
-    '@/*': ['src/*'],
-    '@tests/*': ['tests/*'],
+    '@/*': [ 'src/*' ],
+    '@tests/*': [ 'tests/*' ],
 };
 
 /** @type {import('@jest/types').Config.InitialOptions } */
 module.exports = {
     preset: 'ts-jest/presets/js-with-ts',
     testEnvironment: 'node',
-    transform: {
-        '^.+\\.tsx?$': 'ts-jest',
-    },
+    transform: {'^.+\\.tsx?$': 'ts-jest',},
     testRegex: '(/__test__/.*|/tests/.*|(\\.|/)(test|spec))\\.[tj]sx?$',
-    testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+    testPathIgnorePatterns: [ '/node_modules/', '/dist/' ],
+    moduleFileExtensions: [
+        'ts',
+        'tsx',
+        'js',
+        'jsx',
+        'json'
+    ],
     moduleNameMapper: pathsToModuleNameMapper(tsConfigPaths, { prefix: `${__dirname}/` }),
 
     coverageDirectory: './coverage',
-    coverageReporters: ['html', 'text'],
-    collectCoverageFrom: ['src/**/*.{ts,js}', '!**/node_modules/**', '!**/vendor/**', '!**/dist/**', '!**/tests/**'],
+    coverageReporters: [ 'html', 'text' ],
+    collectCoverageFrom: [
+        'src/**/*.{ts,js}',
+        '!**/node_modules/**',
+        '!**/vendor/**',
+        '!**/dist/**',
+        '!**/tests/**'
+    ],
 };
