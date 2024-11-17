68705e0b08bfdf83f4c5318bd05b3c2cf0cf307f
Patrick Organ <patrick@permafrost.dev>
2024-11-17 17:45:06 -0500

    tooling upgrades

---
+++ eslint.config.js
@@ -0,0 +1,77 @@
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import jestPlugin from 'eslint-plugin-jest';
+import nodePlugin from 'eslint-plugin-node';
+import globals from 'globals';
+import { Linter } from 'eslint';
+import { FlatESLint } from '@typescript-eslint/utils/ts-eslint';
+
+/** @type {Linter.Config} */
+const config = [
+    {
+        files: [ 'src/*.js', 'src/**/*.js', 'src/**/*.ts', 'src/*.ts' ],
+        ignores: [ 'node_modules', 'dist' ],
+        env: {node: true,},
+        languageOptions: {
+            ecmaVersion: 2020,
+            sourceType: 'script',
+            parser: tsParser,
+            parserOptions: {
+                ecmaVersion: 2020,
+                sourceType: 'module',
+            },
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+                ...globals.es2020,
+            },
+        },
+    },
+    {
+        plugins: {
+            '@typescript-eslint': tsPlugin,
+            jest: jestPlugin,
+            node: nodePlugin,
+        },
+        rules: {
+            '@typescript-eslint/ban-ts-comment': 'off',
+            '@typescript-eslint/explicit-function-return-type': 'off',
+            '@typescript-eslint/explicit-module-boundary-types': 'off',
+            '@typescript-eslint/no-empty-function': 'off',
+            '@typescript-eslint/no-empty-interface': 'error',
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/no-inferrable-types': 'off',
+            '@typescript-eslint/no-var-requires': 'off',
+            'jest/no-disabled-tests': 'warn',
+            'jest/no-identical-title': 'error',
+            'newline-per-chained-call': [ 'warn', { ignoreChainWithDepth: 2 }],
+            'node/no-missing-import': 'off',
+            'node/no-missing-require': 'off',
+            'node/no-process-exit': 'off',
+            'node/no-unpublished-import': 'off',
+            'node/no-unpublished-require': 'off',
+            'node/no-unsupported-features/es-syntax': 'off',
+            'array-bracket-newline': [ 'warn', { multiline: true, minItems: 6 }],
+            'array-bracket-spacing': [ 'warn', 'always', { objectsInArrays: false }],
+            'array-element-newline': [ 'warn', { multiline: true, minItems: 6 }],
+            eqeqeq: [ 'error', 'smart' ],
+            indent: [ 'warn', 4, { SwitchCase: 1 }],
+            'no-eval': 'error',
+            'no-var': 'error',
+            'object-curly-newline': [
+                'warn',
+                {
+                    ObjectExpression: { multiline: true, minProperties: 4 },
+                    ObjectPattern: { multiline: true, minProperties: 4 },
+                    ImportDeclaration: 'never',
+                },
+            ],
+        },
+    },
+    new FlatESLint(),
+    // tsPlugin.configs.recommended,
+    // jestPlugin.configs.recommended,
+    // nodePlugin.configs.recommended,
+];
+
+export default config;
