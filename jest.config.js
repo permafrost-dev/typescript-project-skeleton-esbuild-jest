e8a6ef687b6a8100b50bd503ca2b34570a825b19
Patrick Organ <patrick@permafrost.dev>
2024-11-17 17:57:08 -0500

    wip

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,21 @@
-const { pathsToModuleNameMapper } = require('ts-jest');
-//const { compilerOptions } = require('./tsconfig.json');
+import { pathsToModuleNameMapper } from 'ts-jest';
+
 const tsConfigPaths = {
-    '@/*': ['src/*'],
-    '@tests/*': ['tests/*'],
+    '@/*': [ 'src/*' ],
+    '@tests/*': [ 'tests/*' ],
 };
 
 /** @type {import('@jest/types').Config.InitialOptions } */
-module.exports = {
+export default {
     preset: 'ts-jest/presets/js-with-ts',
     testEnvironment: 'node',
     transform: { '^.+\\.tsx?$': 'ts-jest' },
     testRegex: '(/__test__/.*|/tests/.*|(\\.|/)(test|spec))\\.[tj]sx?$',
-    testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+    testPathIgnorePatterns: [ '/node_modules/', '/dist/' ],
+    moduleFileExtensions: [ 'ts', 'tsx', 'js', 'jsx', 'json' ],
     moduleNameMapper: pathsToModuleNameMapper(tsConfigPaths, { prefix: `${__dirname}/` }),
 
     coverageDirectory: './coverage',
-    coverageReporters: ['html', 'text'],
-    collectCoverageFrom: ['src/**/*.{ts,js}', '!**/node_modules/**', '!**/vendor/**', '!**/dist/**', '!**/tests/**'],
+    coverageReporters: [ 'html', 'text' ],
+    collectCoverageFrom: [ 'src/**/*.{ts,js}', '!**/node_modules/**', '!**/vendor/**', '!**/dist/**', '!**/tests/**' ],
 };
